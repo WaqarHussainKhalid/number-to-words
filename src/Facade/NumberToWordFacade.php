753673599d25536e192b0653d0a8f9b21de8ae6b
waqar hussain <waqarhussainkhalid@gmail.com>
2023-05-23 17:30:46 +0500

    laravel number to word functionality

---
+++ src/Facade/NumberToWordFacade.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace waqar\numbertowords\Facade;
+use Illuminate\Support\Facades\Facade;
+
+
+class NumberToWordFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'numberToWords';
+    }
+}
